bcc8ad491348fde34aa96f2650b4f1ed34c8bea9
remirath <rywood71@gmail.com>
2025-06-12 10:43:12 -0400

    fix-favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* point to your Next.js app’s own favicon */}
+        <link rel="icon" href="/favicon-next.ico" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
